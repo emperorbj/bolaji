7bc587569a36140fbe0395d16b201167d184960d
emperorbj <cwaku96@gmail.com>
2024-08-20 13:44:35 +0100

    updated the layout section

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
-import pageTransition from "@/components/pageTransition";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,7 +14,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={inter.className}>
         <Header/>
-        <pageTransition/>
+        
         {children}
         
         </body>
--- components/pageTransition.jsx
+++ components/pageTransition.jsx
@@ -1,21 +1,21 @@
-"use client";
-import { AnimatePresence, motion } from "framer-motion";
-import { usePathname } from "next/navigation";
-const pageTransition = ({children}) => {
-    const pathname = usePathname();
-    return (
-    <AnimatePresence>
-        <div key={pathname}>
-            <motion.div
-            className="h-screen w-screen fixed
-            bg-slate-800 top-0
-            pointer-events-none" 
-            initial={{opacity: 1}} 
-            animate={{opacity:0, transition: {delay:1, duration: 0.4, ease: 'easeInOut'}}}
-            />
-            {children}
-        </div>
-    </AnimatePresence>)
-}
+// "use client";
+// import { AnimatePresence, motion } from "framer-motion";
+// import { usePathname } from "next/navigation";
+// const pageTransition = ({children}) => {
+//     const pathname = usePathname();
+//     return (
+//     <AnimatePresence>
+//         <div key={pathname}>
+//             <motion.div
+//             className="h-screen w-screen fixed
+//             bg-slate-800 top-0
+//             pointer-events-none" 
+//             initial={{opacity: 1}} 
+//             animate={{opacity:0, transition: {delay:1, duration: 0.4, ease: 'easeInOut'}}}
+//             />
+//             {children}
+//         </div>
+//     </AnimatePresence>)
+// }
 
-export default pageTransition
+// export default pageTransition
